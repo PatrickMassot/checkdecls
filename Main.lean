2ee81a0269048010900117b675876a1d8db5883c
Patrick Massot <patrickmassot@free.fr>
2024-02-09 13:42:00 -0500

    Use filename argument

--- Main.lean
+++ Main.lean
@@ -2,15 +2,21 @@ import Lake.CLI.Main
 open Lake Lean
 
 def main (args : List String) : IO UInt32 := do
-  println! "Arguments: {args}"
+  unless args.length == 1 do
+    println! "This commands takes exactly one argument: the path to a file containing a list of declarations to check."
+    return 1
+  let filename : System.FilePath := args[0]!
+  unless ← filename.pathExists do
+    println! "Could not find declaration list {filename}."
+    return 1
   let (elanInstall?, leanInstall?, lakeInstall?) ← findInstall?
   let config ← MonadError.runEIO <| mkLoadConfig.{0} { elanInstall?, leanInstall?, lakeInstall? }
   let ws ← MonadError.runEIO <| (loadWorkspace config).run (.eio .normal)
   let imports := ws.root.leanLibs.concatMap (·.config.roots.map fun module => { module })
   let env ← Lean.importModules imports {}
   let mut ok := true
-  for n in [`Nat, `foo, `bar] do
-    unless env.contains n do
-      println! "{n} is missing"
+  for line in ← IO.FS.lines filename do
+    unless env.contains line.toName do
+      println! "{line} is missing"
       ok := false
   return if ok then 0 else 1
